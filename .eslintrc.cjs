5f40d3ed336b31f21dce29dc5755336ed3a76da6
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2024-02-21 15:50:32 +0200

    Extend .eslintrc.cjs with Typescript support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error'
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser'
+  },
+  plugins: ['@typescript-eslint']
 }
